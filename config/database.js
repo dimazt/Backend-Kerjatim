95a77fa0e9ef7d9945a5a8fe826618883b9b2c1c
Dimas Bayu Sampurno <dimazbex@gmail.com>
2023-06-07 21:13:34 +0800

    Update database.js

--- config/database.js
+++ config/database.js
@@ -1,13 +1,13 @@
 import {Sequelize} from "sequelize";
 
-const db = new Sequelize('kerjatim_db', 'root', '',{
-    host: "localhost",
-    dialect: "mysql",
-    dialectOptions: {
-        timezone: '+08:00', // for reading from database
-        dateStrings: true
-      },
-      timezone: '+08:00', // for writing to database
+const db = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS,{
+  host: process.env.DB_HOST,
+  dialect: "mysql",
+  dialectOptions: {
+      timezone: '+08:00', // for reading from database
+      dateStrings: true
+    },
+    timezone: '+08:00', // for writing to database
 });
 
 export default db;
