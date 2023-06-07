d3d9b0d8721142b024a9c5ff93aad92d183c812c
Dimas Bayu Sampurno <dimazbex@gmail.com>
2023-06-07 21:19:31 +0800

    Update database.js

--- config/database.js
+++ config/database.js
@@ -1,7 +1,7 @@
 import {Sequelize} from "sequelize";
 
-const db = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS,{
-  host: process.env.DB_HOST,
+const db = new Sequelize('kerjatim', 'dimas', '@Sm4rtK1dsrs1B',{
+  host: 'localhost',
   dialect: "mysql",
   dialectOptions: {
       timezone: '+08:00', // for reading from database
